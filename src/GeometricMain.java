a9848b724e54a809649f3bea0a9449e62a56326b
koval-guk <ppp.superkoval@gmail.com>
2022-11-06 00:06:23 +0200

    add task1

--- src/GeometricMain.java
+++ src/GeometricMain.java
@@ -6,19 +6,18 @@ public class GeometricMain {
         GeometricFigure[] figureArray = new GeometricFigure[10];
         Random random = new Random();
         int figureType;
-        int workCharValue;
-        for (int i = 0; i < figureArray.length;i++){
+        for (int i = 0; i < figureArray.length; i++) {
             figureType = random.nextInt(3);
             if (figureType == 0) {
-                figureArray[i] = new Square(random.nextInt(10)+random.nextDouble());
-            } else if (figureType==1){
-                figureArray[i] = new Circle(random.nextInt(10)+random.nextDouble());
-            }else {
-                figureArray[i] = new EquilateralTriangle(random.nextInt(10)+random.nextDouble());
+                figureArray[i] = new Square(random.nextInt(10) + random.nextDouble());
+            } else if (figureType == 1) {
+                figureArray[i] = new Circle(random.nextInt(10) + random.nextDouble());
+            } else {
+                figureArray[i] = new EquilateralTriangle(random.nextInt(10) + random.nextDouble());
             }
-            System.out.println(figureArray[i].getClass()+" "+figureArray[i].area());
+            System.out.println(figureArray[i].getClass() + " " + figureArray[i].area());
         }
-        System.out.println("total area is "+areaSum(figureArray));
+        System.out.println("total area is " + areaSum(figureArray));
     }
 
     public static double areaSum(GeometricFigure[] figureArray) {
